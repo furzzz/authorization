e505f28717b6e91ba9deee7e131b26f126a6390a
furzzz <74243033+furzzz@users.noreply.github.com>
2025-02-01 07:05:52 +0800

    commitPHP2

--- home.php
+++ home.php
@@ -8,7 +8,7 @@ $user = currentUser();
 <html lang="ru" data-theme="light">
 <head>
     <meta charset="UTF-8">
-    <title>AreaWeb - авторизация и регистрация</title>
+    <title>LK</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
     <link rel="stylesheet" href="assets/app.css">
 </head>
